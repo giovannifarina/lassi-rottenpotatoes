9c489aca7e29eda6d8ead87c9dcf2bdbc79c3b95
giovannifarina <gfarina@diag.uniroma1.it>
2022-03-15 11:04:52 +0100

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-=begin    
-    before_action :do_something
-
-    private
-
-    def do_something
-        redirect_to "https://google.com"
-    end
-=end
 end
