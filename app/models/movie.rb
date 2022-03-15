834a4cc804e37e759e54e2b00733b93582a301e4
giovannifarina <gfarina@diag.uniroma1.it>
2022-03-15 10:06:12 +0100

    model validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,25 @@
 class Movie < ApplicationRecord
+
+    @@grandfathered_date = Date.parse('1 Nov 1968')
+
+    def grandfathered?
+        release_date && release_date < @@grandfathered_date
+    end
+
+    def self.all_ratings ; %w[G PG PG-13 R NC-17] ; end #  shortcut: array of strings
+
+    def released_1930_or_later
+        errors.add(:release_date, 'must be 1930 or later') if
+                release_date && release_date < Date.parse('1 Jan 1930')
+    end
+
+    # model validation
+
+	validates :title, :presence => true
+	validates :release_date, :presence => true
+	validate :released_1930_or_later # uses custom validator below
+	validates :rating, :inclusion => {:in => Movie.all_ratings},
+		:unless => :grandfathered?
+
+
 end
