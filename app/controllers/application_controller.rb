27dbb94a61e84a1910100f3724278aadac5f841a
giovannifarina <gfarina@diag.uniroma1.it>
2022-03-15 10:32:22 +0100

    test controller filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+=begin    
+    before_action :do_something
+
+    private
+
+    def do_something
+        redirect_to "https://google.com"
+    end
+=end
 end
