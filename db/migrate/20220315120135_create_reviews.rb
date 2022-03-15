bd9cd1d89fa2bf9629b9124325a01b56ad990e02
giovannifarina <gfarina@diag.uniroma1.it>
2022-03-15 14:54:15 +0100

    incomplete add review model

---
+++ db/migrate/20220315120135_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.integer :potatoes
+      t.text :comments
+      t.belongs_to :movie, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_082820) do
+ActiveRecord::Schema.define(version: 2022_03_15_120135) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -21,4 +21,14 @@ ActiveRecord::Schema.define(version: 2022_03_15_082820) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "potatoes"
+    t.text "comments"
+    t.integer "movie_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  add_foreign_key "reviews", "movies"
 end
