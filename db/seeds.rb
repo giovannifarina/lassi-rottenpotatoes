0233d669141319f1b240d7062b2dfe9f9016c644
Giovanni Farina <gfarina@diag.uniroma1.it>
2022-03-16 16:13:06 +0100

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,18 @@ more_movies = [
       :release_date => '10-Aug-2011'},
     {:title => 'Raiders of the Lost Ark', :rating => 'PG',
       :release_date => '12-Jun-1981'}
-  ]
+]
   
-  more_movies.each do |movie|
-    Movie.create!(movie)
-  end
+more_movies.each do |movie|
+  Movie.create!(movie)
+end
+
+more_moviegoer = [
+	{:name => 'Mario'},
+	{:name => 'Anna'},
+	{:name => 'Gianni'}
+]
+
+more_moviegoer.each do |moviegoer|
+  Moviegoer.create!(moviegoer)
+end
