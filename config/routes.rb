e97974899c903c6571f0db04338d62f2162c4712
Giovanni Farina <gfarina@diag.uniroma1.it>
2022-03-16 11:54:26 +0100

    nested routing for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "movies#index"
-  resources :movies
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :movies do
+		resources :reviews, only: [:new, :create, :destroy]
+	end
 end
