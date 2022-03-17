e10af6fdaa0e59e0c1562aeccd981f103330c545
Giovanni Farina <gfarina@diag.uniroma1.it>
2022-03-17 09:55:10 +0100

    added index moviegoers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :movies do
 		resources :reviews, only: [:new, :create, :destroy]
 	end
-  resources :moviegoers, only: [:new, :show, :create, :destroy]
+  resources :moviegoers, only: [:index, :new, :show, :create, :destroy]
   
 end
